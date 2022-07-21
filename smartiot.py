966d788438d6b226fa8c695b599791fdda838146
Alberto Pacheco <80423661+AlbertoPachecoDev@users.noreply.github.com>
2022-07-21 15:49:03 -0600

    Update smartiot.py
    load_audio() added

--- smartiot.py
+++ smartiot.py
@@ -1,5 +1,21 @@
 # Smart IoT Library
 
+import requests
+import torchaudio
+
 def version():
   ''' Shows Smar IoT library version '''
   print('Smart IoT Library ver. 1.0')
+
+def load_audio(url, fname):
+  '''
+    Retrieve audio file from URL and saves with a filename 
+    Returns audio-wave, sample-rate, metada, bytes-size
+  '''
+  r = requests.get(url)
+  with open(fname, 'wb') as f:
+    f.write(r.content)
+  sz = len(r.content)
+  meta = torchaudio.info(fname)
+  wave, sr = torchaudio.load(fname)
+  return (wave, sr, meta, sz)
