bba393655ea622b0ecbe4de5ca6647461ed7807d
Alberto Pacheco <80423661+AlbertoPachecoDev@users.noreply.github.com>
2022-07-21 16:55:11 -0600

    Update smartiot.py
    print_info() added

--- smartiot.py
+++ smartiot.py
@@ -5,7 +5,7 @@ import torchaudio
 
 def version():
   ''' Shows Smar IoT library version '''
-  print('Smart IoT Library ver. 0.1')
+  print('Smart IoT Library ver. 0.2')
 
 def load_audio(url, fname):
   '''
@@ -19,3 +19,22 @@ def load_audio(url, fname):
   meta = torchaudio.info(fname)
   wave, sr = torchaudio.load(fname)
   return (wave, sr, meta, sz)
+
+def print_info(info, fname=None):
+  ''' Muestra los metadatos de un archivo de audio con la tupla que regresa load_audio'''
+  if fname:
+    print('-' * 30)
+    print('   Filename:', fname)
+    print('-' * 30)
+  wave, sr, meta, sz = info
+  channels, frames = wave.shape
+  print(f'     Frames: {frames}')
+  print(f'   Channels: {channels}')
+  print(f'  File size: {sz} bytes')
+  print(f'Tensor size: {wave.element_size() * channels * frames} bytes')
+  print(f'      Dtype: {wave.dtype}')
+  print(f'        Max: {wave.max().item():6.3f}')
+  print(f'        Min: {wave.min().item():6.3f}')
+  print(f'       Mean: {wave.mean().item():6.3f}')
+  print(f'    Std Dev: {wave.std().item():6.3f}')
+  print(wave)
